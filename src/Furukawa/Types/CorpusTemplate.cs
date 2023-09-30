e6750cb449cc9019bf2d1621b0a317c2c4794ab0
logantgt <loganr.lowe@gmail.com>
2023-09-30 11:22:50 -0600

    More work on kanji cloze card template
    getting this out of the way so I can do Greater things

---
+++ src/Furukawa/Types/CorpusTemplate.cs
@@ -0,0 +1,11 @@
+﻿using Realms;
+
+namespace Furukawa.Types
+{
+    public partial class CorpusTemplate : IRealmObject
+    {
+        [PrimaryKey]
+        public string Type { get; set; } // The main identifier of the template
+        public string Path { get; set; } // Plaintext path to an HTML file which has the template
+    }
+}
